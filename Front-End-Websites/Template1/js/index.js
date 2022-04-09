521539a66e8eb09b6b23e45caa5e9401ae05d635
Douglas254 <dobara@kabarak.ac.ke>
2022-04-09 23:31:15 +0300

    add a click event listener on the menu items to close the Collapse navbar

---
+++ Front-End-Websites/Template1/js/index.js
@@ -0,0 +1,11 @@
+// Use javascript to add a click event listener on the menu items to close the Collapse navbar
+const navLinks = document.querySelectorAll(".nav-item");
+const menuToggle = document.getElementById("navbarNav");
+const bsCollapse = new bootstrap.Collapse(menuToggle, { toggle: false });
+navLinks.forEach((l) => {
+  l.addEventListener("click", () => {
+    bsCollapse.toggle();
+  });
+});
+
+// ***********************************************************
